2600a02599f994634a76bd09120b86b3e6a2ff47
kryo028 <64691538+kryo028@users.noreply.github.com>
2020-06-19 14:45:33 +0900

    Create style.js

---
+++ style.js
@@ -0,0 +1,11 @@
+$('a.link').click(function() {
+  // スクロールの速度
+  var speed = 400; // ミリ秒で記述
+  var href = $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top;
+  $('body,html').animate({
+    scrollTop: position
+  }, speed, 'swing');
+  return false;
+});
